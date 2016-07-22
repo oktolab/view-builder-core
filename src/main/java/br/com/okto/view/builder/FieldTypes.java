a9ec20e0c9a3fba672bef5814aa6f09b2ad37f7a
arthur.pdr@gmail.com <arthur.pdr@gmail.com>
2016-07-22 20:29:15 -0300

    rename

--- src/main/java/br/com/okto/view/builder/FieldTypes.java
+++ src/main/java/br/com/okto/view/builder/FieldTypes.java
@@ -1,6 +1,6 @@
 package br.com.okto.view.builder;
 
-public class FieldValueType {
+public class FieldTypes {
 
 	public static final String STRING = "string";
 	public static final String PASSWORD = "password";
@@ -9,5 +9,6 @@ public class FieldValueType {
 	public static final String NUMBER = "number";
 	public static final String DATE = "date";
 	public static final String COMBO = "combo";
+	public static final String PHONE = "phone";
 	
 }
